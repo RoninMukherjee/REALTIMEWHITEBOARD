49de8d6ebc08e0f0b5be18289439a42a13876aa7
RoninMukherjee <mukherjee.ronin11@gmail.com>
2024-12-01 20:13:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,17 +9,15 @@ const wss = new WebSocketServer({ server });
 
 let canvasState = [];
 
-// Serve React static files
 app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
-// Handle WebSocket connections
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
   ws.on('message', (message) => {
     console.log('Message received:', message);
 
-    // Broadcast to other clients
+  
     wss.clients.forEach((client) => {
       if (client !== ws && client.readyState === 1) {
         client.send(message);
@@ -33,7 +31,7 @@ wss.on('connection', (ws) => {
 
 
 
-  // Send existing canvas state to the new client
+  
   ws.send(JSON.stringify({ type: 'INITIAL_STATE', canvasState }));
 
   ws.on('message', (message) => {
@@ -53,12 +51,11 @@ wss.on('connection', (ws) => {
   });
 });
 
-// Serve the React app for all other routes
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
 
-// Start the server
 const PORT = 4000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
